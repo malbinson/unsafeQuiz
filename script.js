bba0788b9ad8fbd402ee58e2c16bf2066a344b67
malbinson <mattalbinson@berkeley.net>
2018-02-07 18:30:19 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function createQuestions() {
+
+    var parent = document.getElementById("questions")
+    var child = document.createElement("INPUT");
+    child.setAttribute("type", "radio");
+    parent.appendChild(child);
+
+}
