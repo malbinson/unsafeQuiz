980cfd219cfce9b6a482e834aaeb32c31ca27c0e
malbinson <mattalbinson@berkeley.net>
2018-02-08 15:34:20 -0800

    v1

--- script.js
+++ script.js
@@ -1,91 +1,122 @@
-var questionText = ["<br>Are you ready to rock?", "<br>What is your favorite thing?","<br>Are we having fun?"];
 
+var numPages = allQuestions.length - 1;
+var questionsPerPage = 4;
+var totalQuestions = numPages * questionsPerPage;
+
+var numCells = 8;
+var numRadioCells = 6;
+var page = 0;
 
 function createQuestions() {
 
-    var parentDiv = document.getElementById("container");
+    document.getElementById("title").innerHTML = "<h1>Jonah's Quiztacular Spectacular</h1>";
 
-    var qTable = document.createElement("table");
-    qTable.setAttribute("id","qTable");
+    for(var g=0;g<=numPages;g++) {
 
-    //child.setAttribute("type", "radio");
+        var parentDiv = document.getElementById("qSet" + g);
+        var qTable = document.createElement("table");
+        qTable.setAttribute("id","qTable");
+        parentDiv.appendChild(qTable);
 
-    parentDiv.appendChild(qTable);
+        for(var i=0; i<allQuestions[g].length; i++) {
+            var cells = [];
+            var radioCells = [];
+            var qRow = document.createElement("tr");
+            var qCell = document.createElement("td");
+            qCell.setAttribute("colspan",8);
 
-    for(var i=0; i<questionText.length;i++) {
+            qTable.appendChild(qRow);
+            qRow.appendChild(qCell);
+            qRow.setAttribute("class","qRow");
+            qCell.innerHTML = allQuestions[g][i];
 
-        var qRow = document.createElement("tr");
-        var qCell = document.createElement("td");
-        qCell.setAttribute("colspan",8);
+            var aRow = document.createElement("tr");
+            qTable.appendChild(aRow);
 
-        qTable.appendChild(qRow);
-        qRow.appendChild(qCell);
-        qRow.setAttribute("class","qRow");
-        qCell.innerHTML = questionText[i];
+            for(var q=0; q<numCells;q++) {
+                cells.push(document.createElement("td"));
+            }
 
-        var aRow = document.createElement("tr");
-        var aCell1 = document.createElement("td");
-        var aCell2 = document.createElement("td");
-        var aCell3 = document.createElement("td");
-        var aCell4 = document.createElement("td");
-        var aCell5 = document.createElement("td");
-        var aCell6 = document.createElement("td");
-        var aCell7 = document.createElement("td");
-        var aCell8 = document.createElement("td");
+            cells[0].setAttribute("class","edgeCell");
+            cells[0].innerHTML = "Strongly Disagree";
+            cells[7].setAttribute("class","edgeCell");
+            cells[7].innerHTML = "Strongly Agree";
 
-        qTable.appendChild(aRow);
-        aRow.appendChild(aCell1);
-        aRow.appendChild(aCell2);
-        aRow.appendChild(aCell3);
-        aRow.appendChild(aCell4);
-        aRow.appendChild(aCell5);
-        aRow.appendChild(aCell6);
-        aRow.appendChild(aCell7);
-        aRow.appendChild(aCell8);
+            for(var s=0; s<numRadioCells;s++) {
+                radioCells.push(document.createElement("input"));
+                radioCells[s].setAttribute("type","radio");
+                radioCells[s].setAttribute("name","q"+g+i);
+                radioCells[s].setAttribute("value",s+1);
+            }
 
-        aCell1.setAttribute("width","25%");
-        aCell1.innerHTML = "Strongly Disagree";
+            cells[1].appendChild(radioCells[0]);
+            cells[2].appendChild(radioCells[1]);
+            cells[3].appendChild(radioCells[2]);
+            cells[4].appendChild(radioCells[3]);
+            cells[5].appendChild(radioCells[4]);
+            cells[6].appendChild(radioCells[5]);
 
-        var radio1 = document.createElement("input");
-        radio1.setAttribute("type","radio");
-        radio1.setAttribute("name","q"+i);
-        radio1.setAttribute("value","1");
+            for(var d=0;d<numCells;d++) {
+                aRow.appendChild(cells[d]);
+            }
 
-        var radio2 = document.createElement("input");
-        radio2.setAttribute("type","radio");
-        radio2.setAttribute("name","q"+i);
-        radio2.setAttribute("value","2");
-
-        var radio3 = document.createElement("input");
-        radio3.setAttribute("type","radio");
-        radio3.setAttribute("name","q"+i);
-        radio3.setAttribute("value","3");
-
-        var radio4 = document.createElement("input");
-        radio4.setAttribute("type","radio");
-        radio4.setAttribute("name","q"+i);
-        radio4.setAttribute("value","4");
-
-        var radio5 = document.createElement("input");
-        radio5.setAttribute("type","radio");
-        radio5.setAttribute("name","q"+i);
-        radio5.setAttribute("value","5");
-
-        var radio6 = document.createElement("input");
-        radio6.setAttribute("type","radio");
-        radio6.setAttribute("name","q"+i);
-        radio6.setAttribute("value","6");
-
-        aCell8.setAttribute("width","25%");
-        aCell8.innerHTML = "Strongly Agree";
-
-        aCell2.appendChild(radio1);
-        aCell3.appendChild(radio2);
-        aCell4.appendChild(radio3);
-        aCell5.appendChild(radio4);
-        aCell6.appendChild(radio5);
-        aCell7.appendChild(radio6);
-
+        }
     }
 
+    populateButtons();
+    determinePage();
+
+}
+
+function incrementPage(prevOrNext) {
+    if(prevOrNext.value=="prev") {
+        page--;
+    }
+    if(prevOrNext.value=="next") {
+        page++;
+    }
+    populateButtons();
+    determinePage();
+}
+
+function determinePage() {
+
+    for(var k=0;k<=numPages;k++) {
+        document.getElementById("qSet" + k).style.display = "none";
+    }
+    document.getElementById("qSet" + page).style.display = "inline";
+}
+
+function populateButtons() {
+    var btnDiv = document.getElementById("btnSet");
+    btnDiv.innerHTML = "";
+
+    var prev = document.createElement("button");
+    var next = document.createElement("button");
+    var submit = document.createElement("button");
+
+    prev.innerHTML = "< Previous";
+    next.innerHTML = "Next >";
+    submit.innerHTML = "Submit";
+
+    prev.setAttribute("value","prev");
+    prev.setAttribute("onclick","incrementPage(this)");
+    prev.setAttribute("class","btn btn-primary");
+
+    next.setAttribute("value","next");
+    next.setAttribute("onclick","incrementPage(this)");
+    next.setAttribute("class","btn btn-primary");
+
+    submit.setAttribute("onclick","processInput()");
+    submit.setAttribute("class","btn btn-success");
+
+    if(page != 0){
+        btnDiv.appendChild(prev);
+    }
+    if(page != numPages) {
+        btnDiv.appendChild(next);
+    }
+    if(page == numPages) {
+        btnDiv.appendChild(submit);
+    }
 }
